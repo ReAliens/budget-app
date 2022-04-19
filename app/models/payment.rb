7768c476c168ff2e669664ba784e5acc8c04a2c3
Ahmed <ahmedreda152@gmail.com>
2022-04-19 12:33:05 +0200

    add validation

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,7 @@
 class Payment < ApplicationRecord
   has_many :Category, foreign_key: 'payment_id', dependent: :destroy
   belongs_to :user, foreign_key: 'author_id', class_name: 'User'
+
+  validates :name, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :amount, presence: true, comparison: { greater_than: 0 }
 end
