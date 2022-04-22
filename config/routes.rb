f4689a4c23cbe39e7e7d7f6a636da74cb55da53d
Ahmed <ahmedreda152@gmail.com>
2022-04-22 14:07:40 +0200

    add device to application

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
+  devise_scope :user do
+    authenticated :user do
+      root :to => "categories#index", as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => "home#index", as: :unauthenticated_root
+    end
+  end
+
   resources :categories, only: [:index,:show,:new,:create]
-  resources :payments
+  resources :payments, only: [:index,:new,:create]
 end
