eeb05b47b10137d1aac22c7bdb5848fb016a114f
Ahmed <ahmedreda152@gmail.com>
2022-04-23 07:21:37 +0200

    delete confirmation email method

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,16 +43,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    user_name: "ahmedreda152@gmail.com",
-    password: "wvnxyfxijrejxdur",
-    authentication: :plain,
-    enable_starttls_auto: true,
-  }  # Print deprecation notices to the Rails logger.
+  # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
   # Raise exceptions for disallowed deprecations.
